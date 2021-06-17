f06fc6cfd17f0de3d8f7b1301f4e67ea867b056a
Eduardo Margulis <edumarg@gmx.com>
2021-06-17 10:42:24 +0300

    added navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,40 @@
-import { BrowserRouter, Route, Redirect, Switch, Link } from "react-router-dom";
+import React from "react";
+import { Route, Redirect, Switch } from "react-router-dom";
 import RestaurantsList from "./components/restaurantList";
 import Restaurant from "./components/restaurant";
 import Login from "./components/login";
 import NotFound from "./components/notFound";
+import { Navbar, Nav, Image } from "react-bootstrap";
+import { useState } from "react";
+import dish from "./dish.png";
 
 function App() {
+  const { user, setUser } = useState("");
+
+  const logout = () => {
+    setUser("");
+  };
   return (
-    <BrowserRouter>
-      <h1>Hello World!!!</h1>
+    <React.Fragment>
+      <Navbar bg="primary" variant="dark" expand="lg">
+        <Navbar.Brand href="/restaurants">
+          Edu's restaurants reviews{" "}
+          <Image src={dish} alt="dish icon" rounded height="34" width="34" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/restaurants">Restaurants Review</Nav.Link>
+            {!user && <Nav.Link href="/login">Login</Nav.Link>}
+            {user && (
+              <Nav.Link href="/restaurants" onClick={() => logout()}>
+                Logout
+              </Nav.Link>
+            )}
+          </Nav>
+          {user && <Navbar.Text>Signed in as: {user}</Navbar.Text>}
+        </Navbar.Collapse>
+      </Navbar>
       <Switch>
         <Route
           path="/restaurants"
@@ -22,7 +49,7 @@ function App() {
         <Redirect exact from="/" to="/restaurants" />
         <Redirect to="/not-found" />
       </Switch>
-    </BrowserRouter>
+    </React.Fragment>
   );
 }
 
